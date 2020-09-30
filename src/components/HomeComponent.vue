22ab2bbd0b2a642176ed43b6e2973abc8fa3caae
Kayoseo <juan_i_roman@hotmail.com>
2020-09-30 15:47:35 -0300

    Componente home vacio

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,20 @@
+<template src="./HomeComponent.html"></template>
+<script>
+export default {
+  name: 'HomeComponent',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+#home-component{
+position: relative;
+width: 1152px;
+height: 700px;
+
+background: #F9F9F9;
+}
+
+</style>
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import HeaderComponent from './components/HeaderComponent.vue';
+import HomeComponent  from './components/HomeComponent.vue';
+export var Routes=[
+    { path: '/', component: HomeComponent },
+    { path: '/header', component: HeaderComponent }  
+]
